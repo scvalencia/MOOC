fc9ed9ea375fd37952102ec52c8e6147d683b608
cps543 <sc.valencia606@gmail.com>
2015-04-21 12:47:46 -0500

    New file strcture

---
+++ Coursera/Boulder/BeginningGameProgrammingWithC#/BeginningGameProgrammingWithC#/Weeks/Week1/Example1.cs
@@ -0,0 +1,9 @@
+using System; 
+public class Sum {
+	static void Main(String[] args) { 
+		int sum = 0;
+		for (int i=0; i<args.Length; i++)
+		sum += int.Parse(args[i]); 
+		Console.WriteLine("The sum is " + sum);
+	} 
+}
