4cc2b286b3b6ea49d8405529bdb432c8a6f42f8a
cps543 <sc.valencia606@gmail.com>
2015-04-23 13:29:26 -0500

    A little script on C# and its history

--- Coursera/Boulder/BeginningGameProgrammingWithC#/BeginningGameProgrammingWithC#/Weeks/Week1/Example1.cs
+++ Coursera/Boulder/BeginningGameProgrammingWithC#/BeginningGameProgrammingWithC#/Weeks/Week1/Example1.cs
@@ -3,7 +3,7 @@ public class Sum {
 	static void Main(String[] args) { 
 		int sum = 0;
 		for (int i=0; i<args.Length; i++)
-		sum += int.Parse(args[i]); 
+			sum += int.Parse(args[i]); 
 		Console.WriteLine("The sum is " + sum);
 	} 
 }
