29cee4ab3514f8923033581931a3ff502d19a0f2
scvalencia <sc.valencia606@uniandes.edu.co>
2015-12-16 11:06:58 -0500

    Drawing Siepinsky triangles and flowers with Turtle geometry

--- Udacity/Software engineering/ProgrammingFoundationsWithPython/Lesson2/canvas/canvas.py
+++ Udacity/Software engineering/ProgrammingFoundationsWithPython/Lesson2/canvas/canvas.py
@@ -60,8 +60,61 @@ def draw_complex_square(shape, color, speed):
 
 	window.exitonclick()
 
+def draw_flower(shape, color, speed):
+	window = turtle.Screen()
+	window.bgcolor('red')
+
+	t = turtle.Turtle()
+
+	t.shape(shape)
+	t.color(color)
+	t.speed(speed)
+
+	def draw_rhombus():
+		t.left(30)
+		t.forward(100)
+		t.right(60)
+		t.forward(100)
+		t.right(120)
+		t.forward(100)
+		t.left(300)
+		t.forward(100)
+
+	for i in range(18):
+		draw_rhombus()
+		t.right(10)
+
+	t.right(90)
+	t.forward(150 * 2)
+
+	window.exitonclick()
+
+def draw_sierpinski(length,depth):
+	window = turtle.Screen()
+	t = turtle.Turtle()
+
+	if depth==0:
+		for i in range(0, 3):
+			t.fd(length)
+			t.left(120)
+
+	else:
+		draw_sierpinski(length / 2, depth - 1)
+		t.fd(length / 2)
+		draw_sierpinski(length / 2, depth - 1)
+		t.bk(length / 2)
+		t.left(60)
+		t.fd(length / 2)
+		t.right(60)
+		draw_sierpinski(length / 2, depth - 1)
+		t.left(60)
+		t.bk(length / 2)
+		t.right(60)
+
+	window.exitonclick()
+
 def main():
-	draw_complex_square('arrow', 'yellow', 4)
+	draw_sierpinski(100, 2)
 
 if __name__ == '__main__':
 	main()
