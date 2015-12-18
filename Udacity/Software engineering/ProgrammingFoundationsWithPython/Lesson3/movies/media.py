b7e1e12767d08130e8392194123ed44cd66ce735
scvalencia <sc.valencia606@uniandes.edu.co>
2015-12-17 23:29:00 -0500

    Inheritance in python

--- Udacity/Software engineering/ProgrammingFoundationsWithPython/Lesson3/movies/media.py
+++ Udacity/Software engineering/ProgrammingFoundationsWithPython/Lesson3/movies/media.py
@@ -1,6 +1,9 @@
 import webbrowser
 
 class Movie(object):
+	'''A simple class to model the information and behaviour of a movie''' # Movie.__doc__, __name__, __module__
+
+	VALID_RATINGS = ["G", "PG", "PG-13", "R"] # Movie.VALID_RATING
 
 	def __init__(self, title, year, director, story, poster_image_url, youtube_trailer_url):
 		self.title = title
