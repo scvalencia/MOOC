1b2578bbe1a203a824c2b4a53e8146a9186d9e9a
scvalencia <sc.valencia606@uniandes.edu.co>
2016-05-29 12:47:01 -0500

    A modified version of Newton's method ugins scoping and blocks

--- Coursera/Computing/Programming/FunctionalProgrammingPrinciplesInScala/Week1/src/newtonnested.scala
+++ Coursera/Computing/Programming/FunctionalProgrammingPrinciplesInScala/Week1/src/newtonnested.scala
@@ -1,3 +1,5 @@
+def abs(x: Double) = if (x < 0) -x else x
+
 /* A block is delimited by brces, it may contain a sequence of definitions or expressions
  * The last element (expression) of a block defines the value of the block
  * This return expression, can be preceded by auxiliary definitions
@@ -5,23 +7,20 @@
 */
 def sqrt(x: Double) = {
 
-	val EPSILON = 0.000001
-
-	def abs(x: Double) = if (x < 0) -x else x
+	val EPSILON = 0.000001	
 
 	// Recursive definitons need an explicit return type in Scala
-	def newton(guess: Double, x: Double): Double =
-		if (isGoodEnough(guess, x)) guess
-		else newton(improve(guess, x), x)
+	def newton(guess: Double): Double =
+		if (isGoodEnough(guess)) guess
+		else newton(improve(guess))
 
-	def isGoodEnough(guess: Double, x: Double) =
+	def isGoodEnough(guess: Double) =
 		abs(guess * guess - x) / x < EPSILON 
 
-	def improve(guess: Double, x: Double) = 
+	def improve(guess: Double) = 
 		(guess + x / guess) / 2
 
-	newton(1.0, x)
-
+	newton(1.0)
 }
 
 println(sqrt(2))
