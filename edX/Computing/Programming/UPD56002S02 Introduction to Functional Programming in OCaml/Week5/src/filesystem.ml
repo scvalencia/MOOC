a7796129586fb0a6f0a40381b265374a8f37c8a6
scvalencia <sc.valencia606@uniandes.edu.co>
2016-12-24 19:41:47 -0500

    Explring imperative features in OCaml

---
+++ edX/Computing/Programming/UPD56002S02 Introduction to Functional Programming in OCaml/Week5/src/filesystem.ml
@@ -0,0 +1,177 @@
+(*  Displaying a Filesystem Hierarchy  *)
+
+(* 
+	 In this exercise, we will pretty-print directory structures.
+
+	The prelude gives the types that we will use to represent directory structures. A node in the filesystem is 
+	either a simple File, a Directory that contains a nested filesystem, or a Symlink.
+	The latter, as on Unix systems, is a fake file that redirects to another file. For this, it provides the 
+	relative path to this target file. The path is the list of directory to traverse to get to the target file, 
+	followed by the later. If one has to go a directory up, we use the common ".." directory name that represents 
+	the parent directory. A filesystem is a list of named nodes. An example filesystem is given below, in the 
+	format that you will have to produce. Don't worry, we'll break this piece by piece. 
+
+	/photos
+  	| /march
+  	| | photo_1.bmp
+  	| | photo_2.bmp
+  	| | photo_3.bmp
+  	| | index.html
+  	| /april
+  	| | photo_1.bmp
+  	| | photo_2.bmp
+  	| | index.html
+  	/videos
+  	| video1.avi
+  	| video2.avi
+  	| video3.avi
+  	| video4.avi
+  	| best.avi -> video4.avi
+  	| index.html
+  	/indexes
+  	| videos.html -> ../videos/index.html
+  	| photos_march.html -> ../photos/march/index.html
+  	| photos_april.html -> ../photos/april/index.html
+  	| photos_may.html -> INVALID
+
+  	This output was generated from the following OCaml structure. 
+
+  	[ "photos", Dir
+    	[ "march", Dir
+        	[ "photo_1.bmp", File ;
+          	"photo_2.bmp", File ;
+          	"photo_3.bmp", File ;
+          	"index.html", File ] ;
+      	"april", Dir
+        	[ "photo_1.bmp", File ;
+          	"photo_2.bmp", File ;
+          	"index.html", File ] ] ;
+  	"videos", Dir
+		[ "video1.avi", File ;
+		  "video2.avi", File ;
+		  "video3.avi", File ;
+		  "video4.avi", File ;
+		  "best.avi", Symlink [ "video4.avi" ] ;
+		  "index.html", File ] ;
+  	"indexes", Dir
+		[ "videos.html",
+		  Symlink [ ".." ; "videos" ; "index.html" ] ;
+		  "photos_march.html",
+		  Symlink [ ".." ; "photos" ; "march" ; "index.html" ] ;
+		  "photos_april.html",
+		  Symlink [ ".." ; "photos" ; "april" ; "index.html" ] ;
+		  "photos_may.html",
+		  Symlink [ ".." ; "photos" ; "may" ; "index.html" ] ] ]
+*)
+
+type filesystem = (string * node) list
+and node = File
+  		| Dir of filesystem
+  		| Symlink of string list
+
+let example : filesystem = [("photos",
+    Dir
+     [("march",
+       Dir
+        [("photo_1.bmp", File); ("photo_2.bmp", File); ("photo_3.bmp", File);
+         ("index.html", File)]);
+      ("april",
+       Dir
+        [("photo_1.bmp", File); ("photo_2.bmp", File); ("index.html", File)])]);
+   ("videos",
+    Dir
+     [("video1.avi", File); ("video2.avi", File); ("video3.avi", File);
+      ("video4.avi", File); ("best.avi", Symlink ["video4.avi"]);
+      ("index.html", File)]);
+   ("indexes",
+    Dir
+     [("videos.html", Symlink [".."; "videos"; "index.html"]);
+      ("photos_march.html", Symlink [".."; "photos"; "march"; "index.html"]);
+      ("photos_april.html", Symlink [".."; "photos"; "april"; "index.html"]);
+      ("photos_may.html", Symlink [".."; "photos"; "may"; "index.html"])])]
+
+(*
+	Write a function print_path: string list -> unit that prints a relative path (the argument of a Symlink) 
+	and pretty prints it as shown in the example display, using slashes ('/') as separator. 
+*)
+
+let rec print_path parsed_path =
+	match parsed_path with
+		| [] 					-> ()
+		| p::(_::ps as ps')		-> print_string (p ^ "/"); print_path ps'
+		| p::[] 				-> print_string (p)
+
+(*
+	As you can see in the example, the depth of a file in the filesystem (the number of nested folders that are 
+	its ancestors) is represented by a sequence of vertical lines. Write a function 
+	print_file: int -> string -> unit that prints a file name, with the given number of "| " in front of it. 
+*)
+
+let print_file depth filename =
+	let rec aux = fun n -> match n with
+		| 0 	-> ()
+		| n' 	-> print_string "| "; aux (n - 1)
+	in aux depth; print_string filename
+
+(*
+	Write a similar function print_symlink: int -> string -> string list -> unit that prints the link name, 
+	with the given number of "| " in front of it, and the relative path (preceded by an arrow " -> "). 
+*)
+
+let print_symlink depth symname parsed_path =
+	let rec aux = fun n -> match n with
+		| 0 	-> ()
+		| n' 	-> print_string "| "; aux (n - 1)
+	in  aux depth; print_string symname; print_string " -> "; print_path parsed_path
+
+(*
+	Write a similar function print_dir: int -> string -> unit that prints the dir name, with the given number 
+	of "| " in front of it, and the prepended '/'. 
+*)
+
+let print_dir depth dirname =
+	let rec aux = fun n -> match n with
+		| 0 	-> ()
+		| n' 	-> print_string "| "; aux (n - 1)
+	in  aux depth; print_string ("/" ^ dirname)
+
+(*
+	Write a function print_filesystem: filesystem -> unit that traverses the filesystem, producing the same 
+	display as in the example. You will probably need an auxiliary, recursive function, and you will have to 
+	use the previous answers. 
+*)
+
+let print_filesystem root =
+	let rec aux depth = function
+		| (name, File)::rest 			-> print_file depth name; print_newline (); aux depth rest
+		| (name, Dir fs)::rest 			-> print_dir depth name; print_newline (); aux (depth + 1) fs; aux depth rest
+		| (name, Symlink path)::rest 	-> print_symlink depth name path; print_newline (); aux depth rest
+		| []							-> ()
+	in aux 0 root
+
+(*
+	Write a function resolve: string list -> string list -> string list. It takes as parameters:
+
+    1. 
+    	The full path from the root to a symlink, including its name. In the given example, that could be for 
+    	instance [ "indexes" ; "photos_april.html" ].
+    2. 
+    	The relative path for this symlink. Here, that would be [ ".." ; "photos" ; "april" ; "index.html" ]. 
+
+	The function returns the full path from the root to the target of the symlink. Here, we should get 
+	[ "photos" ; "april" ; "index.html" ]. Note that it may not be as easy as it seems, so you may think about 
+	it before plunging into the code. 
+*)
+
+let resolve sym path =
+  	(* 
+  		This pre-completed structure is only here to help you.
+     	If it confuses you, don't hesitate to change it. 
+     *)
+
+  	let rec resolve acc path = match path with
+    	| ".."::rs -> if acc = [] then resolve [] rs else resolve (List.tl acc) rs
+    	| h::rs -> resolve (h::acc) rs
+    	| [] -> List.rev acc 
+
+    in resolve (List.tl (List.rev sym)) path ;;
