0c909894a9c571505f5e364317e9c98a1b1d6fac
scvalencia <sc.valencia606@uniandes.edu.co>
2017-09-18 22:05:51 -0500

    Basic, basic, basic C++

---
+++ Udacity/UD210/lesson01/sizeof.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    cout << sizeof(int) << endl;
+    cout << sizeof(short) << endl;
+    cout << sizeof(long) << endl;
+    cout << sizeof(char) << endl;
+    cout << sizeof(float) << endl;
+    cout << sizeof(double) << endl;
+    cout << sizeof(bool) << endl;
+}
