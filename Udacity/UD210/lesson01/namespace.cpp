ed2c4c63fe595388ae213e1301cb4d8893e96bb0
scvalencia <sc.valencia606@uniandes.edu.co>
2017-09-19 22:06:11 -0500

    Basic code organization using C++

---
+++ Udacity/UD210/lesson01/namespace.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+int main(int argc, char** argv) {
+
+    cout << "Hey, I'm Sebastian. ";
+    cout << "I'm super ready to learn C++\n";
+
+    return 0;
+
+}
