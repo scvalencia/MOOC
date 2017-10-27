f16babb26a62e86417c40f40b38c9ebf800d66c2
scvalencia <sc.valencia606@uniandes.edu.co>
2017-10-27 08:42:29 -0500

    New file structure for the repository

--- Udacity/UD210/lesson01/header.cpp
+++
@@ -1,6 +0,0 @@
-#include "header.hpp"
-
-int main(int argc, char** argv) {
-    cout << "Hello wordl!" << "\n";
-    return 0;
-}
--- Udacity/UD210/lesson01/hello.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-// Compile: g++ hello.cpp -o hello.out
-// Run: ./hello.out
-
-int main(int argc, char** argv) {
-    std::cout << "Hello world, I'm using C++\n";
-    return 0;
-}
