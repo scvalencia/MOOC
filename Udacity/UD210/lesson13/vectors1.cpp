93b8ac46691a8ca7bf493072e96b6fec5ac17ed9
scvalencia <sc.valencia606@uniandes.edu.co>
2017-10-26 22:45:21 -0500

    STL with vectors

--- Udacity/UD210/lesson13/vectors1.cpp
+++ Udacity/UD210/lesson13/vectors1.cpp
@@ -30,6 +30,48 @@ int main(int agrc, char** argv) {
     numbers.push_back(23);
     numbers.push_back(24);
     numbers.push_back(25);
+    numbers.push_back(26);
+    numbers.push_back(27);
+    numbers.push_back(28);
+    numbers.push_back(29);
+
+    for(it = numbers.begin(); it != numbers.end(); it++)
+        std::cout << *it << std::endl;
+
+    std::cout << "==================================================" << std::endl;
+
+    it = numbers.begin();
+    numbers.insert(it + 3, 100);
+
+    for(it = numbers.begin(); it != numbers.end(); it++)
+        std::cout << *it << std::endl;
+
+    std::cout << "==================================================" << std::endl;
+
+    it = numbers.begin();
+    numbers.erase(it + 7);
+    
+    for(it = numbers.begin(); it != numbers.end(); it++)
+        std::cout << *it << std::endl;
+    
+    std::cout << "==================================================" << std::endl;
+
+    it = numbers.begin();
+    numbers.erase(it + 7, it + 10);
+    
+    for(it = numbers.begin(); it != numbers.end(); it++)
+        std::cout << *it << std::endl;
+    
+    std::cout << "==================================================" << std::endl;
+
+    numbers.pop_back();
+    
+    for(it = numbers.begin(); it != numbers.end(); it++)
+        std::cout << *it << std::endl;
+    
+    std::cout << "==================================================" << std::endl;
+
+    numbers.clear();
 
     for(it = numbers.begin(); it != numbers.end(); it++)
         std::cout << *it << std::endl;
